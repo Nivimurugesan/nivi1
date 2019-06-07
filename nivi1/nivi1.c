b367b3aa3a7a88cda37e85f6925145b7eef1a5de
Nivimurugesan <51353330+Nivimurugesan@users.noreply.github.com>
2019-06-07 09:21:29 +0530

    Create nivi1.c

---
+++ nivi1/nivi1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n, rev = 0, red, originalInteger;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    originalInteger = n;
+    while( n!=0 )
+    {
+        red = n%10;
+        rev = rev*10 + red;
+        n /= 10;
+    }
+    if (originalInteger == rev)
+        printf("yes");
+    else
+        printf("no");
+    
+    return 0;
+}
