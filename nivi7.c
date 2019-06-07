69051c53bbad11a5513fc519a4cced59a4a62edf
Nivimurugesan <51353330+Nivimurugesan@users.noreply.github.com>
2019-06-07 10:58:44 +0530

    Create nivi7.c

---
+++ nivi7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n,originalNumber,red,res=0;
+    scanf("%d",&n);
+    originalNumber=n;
+    while(originalNumber!=0)
+    {
+        red=originalNumber%10;
+        res+=red*rer*red;
+        originalNumber/=10;
+    }
+    if(res==n)
+        printf("yes");
+    else
+        printf("no");
+    return 0;
+}
