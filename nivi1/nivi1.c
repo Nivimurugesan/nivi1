80c7c0900af74a77581859ff6fc69e7c7c5c0836
Nivimurugesan <51353330+Nivimurugesan@users.noreply.github.com>
2019-06-07 09:27:36 +0530

    Update nivi1.c

--- nivi1/nivi1.c
+++ nivi1/nivi1.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n, rev = 0, red, originalInteger;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
+    int n,rev=0,red,originalInteger;
+    printf("Enter an integer:");
+    scanf("%d",&n);
     originalInteger = n;
-    while( n!=0 )
+    while(n!=0)
     {
-        red = n%10;
-        rev = rev*10 + red;
-        n /= 10;
+        red=n%10;
+        rev=rev*10+red;
+        n/=10;
     }
-    if (originalInteger == rev)
+    if(originalInteger==rev)
         printf("yes");
     else
         printf("no");
-    
-    return 0;
+        return 0;
 }
