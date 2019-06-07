10be506f0260c16c26ccffb00aaf6b7baf2169d0
Nivimurugesan <51353330+Nivimurugesan@users.noreply.github.com>
2019-06-07 12:47:01 +0530

    Create nivi16.c

---
+++ nivi16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main()
+{
+ int num,temp=0,r,q;
+ scanf("%d",&num);
+ q=n;
+ do
+ {
+      r=q%10;
+      temp=temp+pow(r,2);
+      q=q/10;
+ }
+ while(q%10!=0);
+ printf("%d",t);
+ getch();
+
+
+}
