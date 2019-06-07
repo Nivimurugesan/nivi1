c2e72ca5ac7f79f2b6d474110ca9326833aedaca
Nivimurugesan <51353330+Nivimurugesan@users.noreply.github.com>
2019-06-07 12:40:43 +0530

    Create Nivi15.java

---
+++ Nivi15.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Nivi15{  
+public static void main(String args[]){  
+String s = input.nextLine();
+String s2 = input.nextLine();
+if (s.charAt(0) != s2.charAt(0)) {
+  System.out.println(""+s+ " and "+s2+ " have no common prefix");
+  System.exit(0);
+    }
+if (s.charAt(0) == s2.charAt(0))
+  System.out.print(" "+s.charAt(0));
+if (s.charAt(0) == s2.charAt(0))
+  System.out.print(" "+s.charAt(1));
+if (s.charAt(0) == s2.charAt(0))
+  System.out.print(" "+s.charAt(2));  
+  }
+}
