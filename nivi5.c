662d2e33ea80f377faee1cfb05d42715868d3eb1
Nivimurugesan <51353330+Nivimurugesan@users.noreply.github.com>
2019-06-07 10:20:28 +0530

    Create nivi5.c

---
+++ nivi5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int j,num;
+    scanf("%d", &num);
+    for(i=1; i<=n; i++)
+     {
+        if(i%2 == 0)
+        {
+            printf("%d\n", j);
+        }
+    }
+    return 0;
+}
